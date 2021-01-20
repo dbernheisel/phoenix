0f2f3ee80b8500f2da84bdec5c5541cbe852eb5b
David Bernheisel <david@bernheisel.com>
2021-01-19 22:54:59 -0500

    Try esm wrapper

---
+++ assets/js/phoenix.mjs
@@ -0,0 +1,9 @@
+import * as Phoenix from "./phoenix.js"
+
+export const Socket = Phoenix.Socket
+export const LongPoll = Phoenix.LongPoll
+export const Ajax = Phoenix.Ajax
+export const Presence = Phoenix.Presence
+export const Channel = Phoenix.Channel
+export const Serializer = Phoenix.Serializer
+export default Phoenix
